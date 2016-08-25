09dd56bc366c63f158264e27900f7f18e20d8067
gjones.pf@gmail.com <gjones.pf@gmail.com>
2016-08-25 13:11:43 +1200

    Pull UID from mount, so custom user bind works

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,10 @@
 echo Setting credentials to $USERNAME:$PASSWORD
 PASSWORD=$(perl -e 'print crypt($ARGV[0], "password")' $PASSWORD)
 
+#Take current UID/GID
+UID=$(stat -c '%u' "/ftp")
+GID=$(stat -c '%g' "/ftp")
+
 if [ -n "$PROFTPD_UID" ]; then
     useradd --shell /bin/sh -u $PROFTPD_UID --create-home --password $PASSWORD $USERNAME
 else
